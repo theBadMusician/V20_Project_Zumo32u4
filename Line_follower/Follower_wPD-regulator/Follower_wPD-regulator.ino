2ed03222207287a03195b540dee035d1d5fd46e0
Benjaminas Visockis <benjamvi@stud.ntnu.no>
2020-02-29 15:52:25 +0100

    Calibrated the PD-terms and cruise control. Added some comments (Norwegian).

--- Line_follower/Follower_wPD-regulator/Follower_wPD-regulator.ino
+++ Line_follower/Follower_wPD-regulator/Follower_wPD-regulator.ino
@@ -18,6 +18,9 @@
 #include <Zumo32U4.h>
 #include <stdio.h>
 
+#define DEBUG 0 		// Sett til sant for minimal debugging.
+#define LOGCCONTROL 0	// Velge mellom logaritmisk eller eksponentiel fartskontroll.
+
 Zumo32U4LCD 				lcd;
 Zumo32U4ButtonA 			buttonA;
 Zumo32U4LineSensors 		lineSensors;
@@ -29,24 +32,30 @@ uint16_t	lineSensorValues[NUM_SENSORS];
 bool 		useEmitters = true;
 uint8_t 	selectedSensorIndex = 0;
 
-#define  	proportional_term  0.9
+// Disse verdiene kan eksperimenteres med og kalibreres bedre.
+#define  	proportional_term  0.3  //0.5 funker bra, 0.3 enda bedre, 0.25 også, 0.1 starter å sakte ned.
 #define		derivative_term  9.0
-#define		sample_rate 0
-#define		cc_definition 600
+#define		sample_rate 1
+#define		cc_definition 10000 // 1500, 3000, 10000 funker bra, høyere verdier gjør at dronen sakter.
+#define		cc_speed 300
 
+// En array for avvik + en sjekk for hvor mange elementer i arrayen.
 double 		error[5] = {0, 0, 0, 0, 0};
 int16_t 	num_of_errors = sizeof(error)/sizeof(int);
 
+// Kontrollvariabler.
 int16_t 	left_speed,
 			right_speed,
 			turn_control,
 			cruise_control;
 
+// Tidsvariabler.
 uint32_t	check_time_calibrate,
 			check_time_errors[5],
 			check_time_lcd;
 
 
+// Kalibrerer sensorene i 4 sekunder.
 void calibrate_sensors() {
 	lcd.clear();
 
@@ -83,35 +92,50 @@ void setup() {
 
 
 void loop() {
-	// char buffer[100];
-	// sprintf(buffer, "%d   %d   %d   %d   %d", error[0], error[1], error[2], error[3], error[4]);
-	// Serial.println(buffer);
+	
+	#if DEBUG
+		char buffer[100];
+		sprintf(buffer, "%d   %d   %d   %d   %d", error[0], error[1], error[2], error[3], error[4]);
+		Serial.println(buffer);
+	#endif
 
+	// IR-sensorene leses.
 	int pos = lineSensors.readLine(lineSensorValues);
 
+	// Førrige avviker oppdateres.
 	for (uint8_t i = 4; i > 0; i--) {
-		if(millis() - check_time_errors[i] > sample_rate) {
+		if(micros() - check_time_errors[i] > sample_rate) {
 			error[i] = error[i - 1];
-			check_time_errors[i] = {millis()};
+			check_time_errors[i] = {micros()};
 		}
 	}
 
 	error[0] = map(pos, 0, 4000, 400, -400);
 
+	// Fartskontrollsystemet. Jo lengere unna linja dronen peker, jo saktere kjør den.
+	// Det gir bedre tid på å utføre vinkeljusteringer.
 	uint16_t cruise_control = map(abs(error[0]), 0, 400, cc_definition, 0);
-	// cruise_control = log(cruise_control + 1)/log(cc_definition) * 250;
-	cruise_control = pow(cruise_control, 2)/pow(cc_definition, 2) * 250;
+	#if LOGCCONTROL
+		cruise_control = log(cruise_control + 1)/log(cc_definition) * cc_speed;
+	#else
+		cruise_control = pow(cruise_control, 2)/pow(cc_definition, 2) * cc_speed;
+	#endif
 
+	// Kalkulasjoner for PD-regulator. Avviket ganges med en konstant verdi i første parantes.
+	// Snittendringsrate mellom nåværende avvik og et førrige avvik finnes i den andre parantes. Så adderes dem.
 	int16_t turn_control = (error[0] * proportional_term) + (derivative_term * ((error[0] - error[5]) / num_of_errors));
 
+	// Alle regulatorene summeres.
 	left_speed = 100 + cruise_control + turn_control;
 	right_speed = 100 + cruise_control - turn_control;
 	
+	// Farten begrenses.
 	left_speed = constrain(left_speed, -400, 400);
 	right_speed = constrain(right_speed, -400, 400);
 
 	motor.setSpeeds(right_speed, left_speed);
 	
+	// Skrive avlesingen fra linje sensorene og fartene til LDC skjerm.
 	if (millis() - check_time_lcd > 100) {
 		lcd.clear();
 		lcd.gotoXY(0,0);
